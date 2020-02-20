4edc556caa3d1f899e63aee4ab606cedc6ffa8f1
Carmen Canedo <carmen.canedo98@gmail.com>
2020-02-19 20:35:11 -0500

    calculted factorials with a for loop

---
+++ factorial.py
@@ -0,0 +1,22 @@
+# factorial.py
+#   Program to comput the factorial of a number
+#   Illustrates for loop with an accumulator
+
+def main():
+
+    # Prompting user to input a number
+    number = int(input("Please input a whole number: "))
+
+    # Initializing factorial
+    factorial = 1
+
+    # Calculating factorial
+    for i in range(number, 1, -1):
+        factorial = factorial * number
+
+    # Printing solution
+    print("The factorial of", number, "is", factorial)
+
+
+if __name__ == '__main__':
+    main()
