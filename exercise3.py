b37d32fcf52e17cd825ef1ee59bcbca41e0cad9e
Carmen Canedo <carmen.canedo98@gmail.com>
2020-02-19 21:05:34 -0500

    set up exercise 3

---
+++ exercise3.py
@@ -0,0 +1,13 @@
+# exercise3
+#   A program to compute the molecular weight of a carbohydrate
+#   Measured in grams per mole
+#
+#   Ask for number of hydrogen atoms
+#   Ask for number of carbon atoms
+#   Ask for number of oxygen atoms
+#   Print the total combined molecular weight of all atoms
+#
+#   H: 1.00794
+#   C: 12.0107
+#   O: 15.9994
+#   General forumal for carbohydrate: CH20
