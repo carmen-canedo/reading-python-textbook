456954f676952d60c563c34e52352b2532b576d2
Carmen Canedo <carmen.canedo98@gmail.com>
2020-02-27 15:23:18 -0500

    finished overall structure of example 1

--- example1.py
+++ example1.py
@@ -25,25 +25,40 @@ class Projectile:
         self.xvel = velocity * math.cos(theta)
         self.yvel = velocity * math.sin(theta)
 
-        # Methods
-        # Return current position
-        def get_x():
-            return self.xpos
+    # Methods
+    # Return current position
+    def get_x(self):
 
-        def get_y():
-            return self.ypos
+        return self.xpos
 
-        # Updates the state of projectile over time
-        def update(self, time):
-            self.xpos = self.xpos + time * self.xvel
-            yvel1 = self.yvel - time * 9.8
-            self.ypos = self.ypos + time * (self.yvel + yvel1) / 2.0
-            self.yvel = yvel1
+    def get_y(self):
+        return self.ypos
+
+    # Updates the state of projectile over time
+    def update(self, time):
+        self.xpos = self.xpos + time * self.xvel
+        yvel1 = self.yvel - time * 9.8
+        self.ypos = self.ypos + time * (self.yvel + yvel1) / 2.0
+        self.yvel = yvel1
+
+# Prompting user for values
+def get_inputs():
+    a = float(input("Enter the launch angle (in degrees): "))
+    v = float(input("Enter the initial velocity (m/s): "))
+    h = float(input("Enter the initial height (m): "))
+    t = float(input("Enter the time interval b/n position calculations: "))
+    return a, v, h, t
 
 # Calling main function
 def main():
 
+    angle, vel, h0, time = get_inputs()
     cball = Projectile(angle, vel, h0)
+
+    while (cball.get_y() >= 0):
+        cball.update(time)
+
+    print("\nDistance traveled: {0:0.1f} meters.".format(cball.get_x()))
 
 if __name__ == '__main__':
     main()
