349cbb5c0e50bcff7b1a3214b8307aaee8f29768
Carmen Canedo <carmen.canedo98@gmail.com>
2020-02-27 22:39:03 -0500

    finished reading through gpa example

--- gpa.py
+++ gpa.py
@@ -1,6 +1,7 @@
-# gpa.property
-#   Calculating the gpa of a student
+# gpa.py
+#   Program to find student with highest GPA
 
+# Creating class containing student information
 class Student:
 
     # Initializing instance variables
@@ -23,18 +24,38 @@ class Student:
         return self.qpoints
 
     # Calculates the student's GPA
-    def get_gpa(self):
-        return self.qpoints / self.hours 
+    def gpa(self):
+        return self.qpoints / self.hours
+
+def make_student(infoStr):
+    # infoStr is a tab-separated line: name hours qpoints
+    # returns a corresponding Student object
+    name, hours, qpoints = infoStr.split("\t")
+    return Student(name, hours, qpoints)
 
 # Calling main function
 def main():
-
+    # Testing out making an object
     # Creating student object
     student_1 = Student("Adams Henry", 127, 228)
 
     #Printing the student's name
     print(student_1.get_name())
 
+    # Open the input file of rreading
+    filename = input("Enter the name of the grade file: ")
+    infile = open(filename, "r")
+
+    # Set best to the record for the first student in the file
+    best = make_student(infile.readline())
+
+    # Process subsequent lines of the file
+    for line in infile:
+        # Turn the line into a student record
+        s = make_student(line)
+        # If this student is best so far, remember it
+        if s.gpa() > best.gpa():
+            best = s 
 
 
 if __name__ == '__main__':
