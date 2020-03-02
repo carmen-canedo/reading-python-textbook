a184b4c376206accce95851d2b1debfac07ef0d8
Carmen Canedo <carmen.canedo98@gmail.com>
2020-03-02 17:16:18 -0500

    worked on methods for dieview

--- dieView.py
+++ dieView.py
@@ -30,7 +30,7 @@ class DieView:
         # Create 7 circles for standard pip locations
         self.pip1 = self.__makePip((cx - offset), (cy - offset))
         self.pip2 = self.__makePip((cx - offset), cy)
-        self.pip3 = self.__makePip((cx - offset), (cy + offset)
+        self.pip3 = self.__makePip((cx - offset), (cy + offset))
         self.pip4 = self.__makePip(cx, cy)
         self.pip5 = self.__makePip((cx + offset), (cy - offset))
         self.pip6 = self.__makePip((cx + offset), cy)
@@ -45,4 +45,44 @@ class DieView:
         pip.setFill(self.background)
         pip.setOutline(self.background)
         pip.draw(self.win)
-        return pip 
+        return pip
+
+    def setValue(self, value):
+        "Set this die to display value."
+        # Turn all pips off
+        self.pip1.setFill(self.background)
+        self.pip2.setFill(self.background)
+        self.pip3.setFill(self.background)
+        self.pip4.setFill(self.background)
+        self.pip5.setFill(self.background)
+        self.pip6.setFill(self.background)
+        self.pip7.setFill(self.background)
+
+        # Turns correct pips on
+        if (value == 1):
+            self.pip4.setFill(self.foreground)
+        elif (value == 2):
+            self.pip1.setFill(self.foreground)
+            self.pip7.setFill(self.foreground)
+        elif (value == 3):
+            self.pip1.setFill(self.foreground)
+            self.pip7.setFill(self.foreground)
+            self.pip4.setFill(self.foreground)
+        elif (value == 4):
+            self.pip1.setFill(self.foreground)
+            self.pip3.setFill(self.foreground)
+            self.pip5.setFill(self.foreground)
+            self.pip7.setFill(self.foreground)
+        elif (value = 5):
+            self.pip1.setFill(self.foreground)
+            self.pip3.setFill(self.foreground)
+            self.pip4.setFill(self.foreground)
+            self.pip5.setFill(self.foreground)
+            self.pip7.setFill(self.foreground)
+        else:
+            self.pip1.setFill(self.foreground)
+            self.pip2.setFill(self.foreground)
+            self.pip3.setFill(self.foreground)
+            self.pip5.setFill(self.foreground)
+            self.pip6.setFill(self.foreground)
+            self.pip7.setFill(self.foreground)
