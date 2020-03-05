c53b0d084898bbaa727bc5401c77bdbb115c53f5
Carmen Canedo <carmen.canedo98@gmail.com>
2020-03-04 22:15:35 -0500

    working on input dialog class

--- animation.py
+++ animation.py
@@ -39,6 +39,33 @@ class ShotTracker:
         """ Undraw the shot """
         self.marker.undraw()
 
+
+class InputDialog:
+
+    """ A custom window for getting simulation values (angle, velocity,
+    and height) from the user. """
+
+    def __init__(self, angle, vel, height):
+        """ Build and display the input window. """
+
+        self.win = win = graphics.GraphWin("Initial Values", 200, 300)
+        win.setCoords(0, 4.5, 0.5)
+
+        graphics.Text(graphics.Point(1, 1), "Angle").draw(win)
+        self.angle = graphics.Entry(graphics.Point(3, 1), 5).draw(win)
+        self.angle.graphics.setText(str(angle))
+
+        graphics.Text(graphics.Point(1, 2), "Velocity").draw(win)
+        self.vel = graphics.Entry(graphics.Point(3, 2), 5).draw(win)
+        self.vel.graphics.setText(str(vel))
+
+        graphics.Text(graphics.Point(1, 3), "Height").draw(win)
+        self.height = graphics.Entry(graphics.Point(3, 3), 5).draw(win)
+        self.height.graphics.setText(str(height))
+
+        # Stopped at sel.fire
+
+
 # Calling main function
 def main():
 
